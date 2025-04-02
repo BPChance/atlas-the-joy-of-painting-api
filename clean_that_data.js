6531d609e7fa097fa98f5b60b38a70f502cd19ec
BPChance <bradenpchance6@gmail.com>
2025-04-02 14:56:02 -0500

    file for modifying csvs to be consistent across the files, working on it

---
+++ clean_that_data.js
@@ -0,0 +1,33 @@
+const csv = require('csv-parser');
+const papa = require('papaparse');
+const fs = require('fs');
+
+const colorsUsed = 'The Joy Of Painting - Colors Used';
+const subjectMatter = 'The Joy Of Painting - Subject Matter';
+const episodeDates = 'The Joy Of Painting - Episode Dates';
+
+function cleanData(fileName, outputFileName) {
+  // read csv files
+  const cleanedData = [];
+
+  fs.createReadStream(fileName)
+    .pipe(csv())
+    .on('data', (row) => {
+      if (row.TITLE) {
+        // remove triple quote and make lowercase
+        row.TITLE = row.TITLE.replace(/^["']+|["']+$/g, '').toLowerCase();
+        // capitalize first letter of each word in title
+        row.TITLE = row.TITLE.replace(/\b\w/g, (char) => char.toUpperCase());
+      }
+      cleanedData.push(row);
+    })
+    .on('end', () => {
+      console.log(`${fileName} cleaned`);
+
+      const csvOutput = papa.unparse(cleanedData);
+      fs.writeFileSync(outputFileName, csvOutput);
+      console.log(`cleaned data saved to ${outputFileName}`);
+    });
+}
+
+cleanData(subjectMatter, 'subjectMatterCleaned.csv');
