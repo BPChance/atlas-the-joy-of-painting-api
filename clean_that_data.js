2d6d57a27283c5662e2ef2d7d64b5a3de2b97333
BPChance <bradenpchance6@gmail.com>
2025-04-07 16:37:43 -0500

    complete cleaning script

--- clean_that_data.js
+++ clean_that_data.js
@@ -3,11 +3,11 @@ const papa = require('papaparse');
 const fs = require('fs');
 const titleCase = require('title-case').titleCase;
 
-const colorsUsed = 'The Joy Of Painting - Colors Used';
-const subjectMatter = 'The Joy Of Painting - Subject Matter';
-const episodeDates = 'The Joy Of Painting - Episode Dates';
+const colorsUsed = 'datasets/The Joy Of Painting - Colors Used';
+const subjectMatter = 'datasets/The Joy Of Painting - Subject Matter';
+const episodeDates = 'datasets/The Joy Of Painting - Episode Dates.csv';
 
-function cleanData(fileName, outputFileName) {
+function cleanSubjectData(fileName, outputFileName) {
   // read csv files
   const cleanedData = [];
 
@@ -17,18 +17,74 @@ function cleanData(fileName, outputFileName) {
       if (row.TITLE) {
         // remove triple quote and make lowercase
         row.TITLE = row.TITLE.replace(/^["']+|["']+$/g, '').toLowerCase();
-        // capitalize first letter of each word in title
+        // consistent title case - subject matter
         row.TITLE = titleCase(row.TITLE);
       }
+
       cleanedData.push(row);
     })
     .on('end', () => {
-      console.log(`${fileName} cleaned`);
-
       const csvOutput = papa.unparse(cleanedData);
+
       fs.writeFileSync(outputFileName, csvOutput);
       console.log(`cleaned data saved to ${outputFileName}`);
     });
 }
 
-cleanData(subjectMatter, 'subjectMatterCleaned.csv');
+function cleanColorData(fileName, outputFileName) {
+  // read csv files
+  const cleanedData = [];
+
+  fs.createReadStream(fileName)
+    .pipe(csv())
+    .on('data', (row) => {
+      // consistent title case for - colors used
+      if (row.TITLE) {
+        row.TITLE = titleCase(row.TITLE);
+      }
+
+      cleanedData.push(row);
+    })
+    .on('end', () => {
+      const csvOutput = papa.unparse(cleanedData);
+
+      fs.writeFileSync(outputFileName, csvOutput);
+      console.log(`cleaned data saved to ${outputFileName}`);
+    });
+}
+
+function cleanDateData(fileName, outputFileName) {
+  const cleanedData = [];
+
+  fs.createReadStream(fileName)
+    .pipe(csv())
+    .on('data', (row) => {
+      if (row.TITLE) {
+        row.TITLE = titleCase(row.TITLE);
+      }
+
+      if (row.date) {
+        row.date = row.date.replace(/[()]/g, '');
+
+        const parsedDate = new Date(row.date);
+        if (!isNaN(parsedDate)) {
+          row.date = parsedDate.toISOString().split('T')[0];
+        } else {
+          console.warn(`Invalid date format: ${row.date}`);
+          row.date = null;
+        }
+      }
+
+      cleanedData.push(row);
+    })
+    .on('end', () => {
+      const csvOutput = papa.unparse(cleanedData);
+
+      fs.writeFileSync(outputFileName, csvOutput);
+      console.log(`cleaned data saved to ${outputFileName}`);
+    });
+}
+
+cleanSubjectData(subjectMatter, 'subjectMatterCleaned.csv');
+cleanColorData(colorsUsed, 'colorsUsedCleaned.csv');
+cleanDateData(episodeDates, 'dateDataCleaned.csv');
