34184a7b19cd98e318987217063219fa67d3254c
BPChance <bradenpchance6@gmail.com>
2025-04-24 13:19:18 -0500

    server.js for starting the server

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const app = require('./src/app');
 const port = process.env.PORT || 3000;
 
+// start the server
 app.listen(port, () => {
   console.log(`API server running on http://localhost:${port}`);
 });
