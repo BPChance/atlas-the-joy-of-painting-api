431f5f386018d8f41f98ed70bd2a61566daca848
BPChance <bradenpchance6@gmail.com>
2025-04-24 13:20:48 -0500

    This file defines the routes for the episodes API

--- src/routes/episodes_routes.js
+++ src/routes/episodes_routes.js
@@ -1,7 +1,7 @@
+// This file defines the routes for the episodes API
 const express = require('express');
 const router = express.Router();
 const episodeController = require('../controllers/episodes_controller');
 
 router.get('/', episodeController.getEpisodes);
-
 module.exports = router;
